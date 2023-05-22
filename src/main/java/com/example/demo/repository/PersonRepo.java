e64594b3fbe2fb8bc46ab172366602080ca26340
OmiJ80 <121397278+OmiJ80@users.noreply.github.com>
2023-05-22 15:08:16 +0530

    Delete src directory

--- src/main/java/com/example/demo/repository/PersonRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Person;
-
-public interface PersonRepo extends JpaRepository<Person, Integer> {
-    public Person findByUsername(String username);
-}
